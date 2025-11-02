4d1e5cb582938697d6b1922c0f80d55b1b3f89a6
jingyuan <ljy1340691470@gmail.com>
2025-11-02 13:12:24 +0100

    (fix) delete useless loss functions (ari and regu loss)

--- phc/learning/amp_agent.py
+++ phc/learning/amp_agent.py
@@ -908,30 +908,6 @@ class AMPAgent(common_agent.CommonAgent):
                 vq_loss = extra_dict['loss']  # 已包含 codebook + commitment
                 info_dict["kin_vq_loss"] = vq_loss
 
-                # ----------- AR1 连续性约束（可选）-----------
-                ar1_prior = 0
-                if humanoid_env.use_ar1_prior:
-                    time_zs = extra_dict['quantized_z_out'].view(
-                        self.minibatch_size // self.horizon_length, self.horizon_length, -1
-                    )
-                    phi = 0.99
-                    error = time_zs[:, 1:] - time_zs[:, :-1] * phi
-                    idxes = kin_dict['progress_buf'].view(self.minibatch_size // self.horizon_length,
-                                                          self.horizon_length, -1)
-                    not_consecs = ((idxes[:, 1:] - idxes[:, :-1]) != 1).view(-1)
-                    error = error.view(-1, error.shape[-1])
-                    error[not_consecs] = 0
-                    starteres = ((idxes <= 2)[:, 1:] + (idxes <= 2)[:, :-1]).view(-1)
-                    error[starteres] = 0
-                    ar1_prior = torch.norm(error, dim=-1).mean()
-                    info_dict["kin_ar1"] = ar1_prior
-
-                # ----------- 正则项 -----------
-                z_q = extra_dict['quantized_z_out']
-                z_b = extra_dict['z_before_quant']
-                regu_prior = ((z_q ** 2).mean() + (z_b ** 2).mean()) * 0.001
-                info_dict["kin_prior_regu"] = regu_prior
-
                 # ----------- 总损失函数 -----------
                 kin_loss = (
                         kin_action_loss
@@ -944,8 +920,6 @@ class AMPAgent(common_agent.CommonAgent):
                     print("!!! 致命错误: 训练损失（kin_loss）变成了 nan 或 inf !!!")
                     print(f"kin_action_loss: {kin_action_loss}")
                     print(f"vq_loss: {vq_loss}")
-                    print(f"ar1_prior: {ar1_prior}")
-                    print(f"regu_prior: {regu_prior}")
                     import ipdb;
                     ipdb.set_trace()  # 在这里停住
 
