4feffb6f314e4fdf00310bcff7d7749d89b36186
jingyuan <ljy1340691470@gmail.com>
2025-11-04 11:18:31 +0100

    (test_fin) return original VQ loss weight and state ari loss weight

--- phc/learning/amp_agent.py
+++ phc/learning/amp_agent.py
@@ -949,9 +949,9 @@ class AMPAgent(common_agent.CommonAgent):
                 # ----------- 总损失函数 -----------
                 kin_loss = (
                         kin_action_loss
-                        + vq_loss * getattr(humanoid_env, "vq_coeff", 0.1)
+                        + vq_loss * getattr(humanoid_env, "vq_coeff", 1)
                         # + ar1_prior * humanoid_env.ar1_coefficient
-                        + state_smooth_loss * getattr(humanoid_env, "state_smooth_coeff", 0.5)
+                        + state_smooth_loss * getattr(humanoid_env, "state_smooth_coeff", 0.1)
                         # + regu_prior * 0.005
                 )
 
