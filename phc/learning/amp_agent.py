3322fc161306ce189851c1f8f8097e4a86703342
jingyuan <ljy1340691470@gmail.com>
2025-11-10 00:38:27 +0100

    (test) recover AR1 manifold; only update last frame action loss

--- phc/learning/amp_agent.py
+++ phc/learning/amp_agent.py
@@ -933,28 +933,29 @@ class AMPAgent(common_agent.CommonAgent):
                         gt_action_full[i, -eff_len:, :] = gt_action[i-eff_len+1:i+1, :]
                 pred_action, _, extra_dict = self.model.a2c_network.eval_actor(batch_dict, return_extra=True)
                 # ----------- 动作重建损失 -----------
-                kin_action_loss = ((pred_action - gt_action_full).norm(dim=-1) * effective_mask.detach()).sum() / effective_mask.sum()
+                kin_action_loss = torch.norm(pred_action[:,-1,:] - gt_action, dim=-1).mean()
+                # kin_action_loss = ((pred_action - gt_action_full).norm(dim=-1) * effective_mask.detach()).sum() / effective_mask.sum()
 
                 # ----------- 从模型中直接拿 VQ 损失 -----------
                 vq_loss = extra_dict['loss']  # 已包含 codebook + commitment
                 info_dict["kin_vq_loss"] = vq_loss
 
                 # ----------- AR1 连续性约束（可选）-----------
-                # ar1_prior = 0
-                # if humanoid_env.use_ar1_prior:
-                #     time_zs = extra_dict['quantized_z_out'].view(
-                #         self.minibatch_size // self.horizon_length, self.horizon_length, -1
-                #     )
-                #     error = time_zs[:, 1:] - time_zs[:, :-1]
-                #     idxes = kin_dict['progress_buf'].view(self.minibatch_size // self.horizon_length,
-                #                                           self.horizon_length, -1)
-                #     not_consecs = ((idxes[:, 1:] - idxes[:, :-1]) != 1).view(-1)
-                #     error = error.view(-1, error.shape[-1])
-                #     error[not_consecs] = 0
-                #     starteres = ((idxes <= 2)[:, 1:] + (idxes <= 2)[:, :-1]).view(-1)
-                #     error[starteres] = 0
-                #     ar1_prior = torch.norm(error, dim=-1).mean()
-                #     info_dict["kin_ar1"] = ar1_prior
+                ar1_prior = 0
+                if humanoid_env.use_ar1_prior:
+                    time_zs = extra_dict['quantized_z_out'].view(
+                        self.minibatch_size // self.horizon_length, self.horizon_length, -1
+                    )
+                    error = time_zs[:, 1:] - time_zs[:, :-1]
+                    idxes = kin_dict['progress_buf'].view(self.minibatch_size // self.horizon_length,
+                                                          self.horizon_length, -1)
+                    not_consecs = ((idxes[:, 1:] - idxes[:, :-1]) != 1).view(-1)
+                    error = error.view(-1, error.shape[-1])
+                    error[not_consecs] = 0
+                    starteres = ((idxes <= 2)[:, 1:] + (idxes <= 2)[:, :-1]).view(-1)
+                    error[starteres] = 0
+                    ar1_prior = torch.norm(error, dim=-1).mean()
+                    info_dict["kin_ar1"] = ar1_prior
                 # ----------- AR1 连续性约束 for state -----------
                 pred_state = extra_dict['state_after_quant']
                 # reshape to [B, T, state_dim]
@@ -981,7 +982,7 @@ class AMPAgent(common_agent.CommonAgent):
                 kin_loss = (
                         kin_action_loss
                         + vq_loss * getattr(humanoid_env, "vq_coeff", 1)
-                        # + ar1_prior * humanoid_env.ar1_coefficient
+                        + ar1_prior * humanoid_env.ar1_coefficient
                         + state_smooth_loss * getattr(humanoid_env, "state_smooth_coeff", 0.1)
                         + regu_prior * 0.005
                 )
